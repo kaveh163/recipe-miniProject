4c281793d91b3d58ace46d672b22466efeb59e26
Kaveh <kavehfrounchi@gmail.com>
2021-06-13 11:49:10 +0430

    Finished adding localstorage and updating order of food items in admin route

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const multer = require('multer');
 const passport = require('passport');
 const db = require("./models");
 
+if (typeof localStorage === "undefined" || localStorage === null) {
+    var LocalStorage = require('node-localstorage').LocalStorage;
+    localStorage = new LocalStorage('./scratch');
+}
 const app = express();
 
 
@@ -55,8 +59,19 @@ const upload = multer({
 
 app.post('/thanks', upload.single('avatar'), function (req, res) {
     count++;
+
+    //Local Storage for count2
+    let count2 = JSON.parse(localStorage.getItem("count"));
+    if (count2 === null) {
+        count2 = 1;
+        localStorage.setItem("count", JSON.stringify(count2));
+    } else {
+        count2++;
+        localStorage.setItem("count", JSON.stringify(count2));
+    }
+
     const obj = {};
-    obj['id'] = count;
+    obj['id'] = JSON.parse(localStorage.getItem("count"));
     obj['food'] = req.body.foodName;
     obj['image'] = req.file.path;
     obj['altName'] = req.file.originalname;
@@ -75,6 +90,19 @@ app.post('/thanks', upload.single('avatar'), function (req, res) {
     obj['user'] = req.user.firstName;
     imgStore.push(obj);
     console.log('imgStore', imgStore);
+
+    // Local Storage
+    let foodArr = JSON.parse(localStorage.getItem("food"));
+    if (foodArr === null) {
+        foodArr = [];
+        foodArr.push(obj);
+        localStorage.setItem('food', JSON.stringify(foodArr));
+    } else {
+        foodArr.push(obj);
+        localStorage.setItem('food', JSON.stringify(foodArr));
+    }
+
+
     // console.log('result',result);
     if (arrStore.length === 0) {
         // arrStore = [...arrStore, ...result];
@@ -120,14 +148,21 @@ app.get('/home', function (req, res) {
     //     <img src=${imgStore[0].image} alt=${imgStore[0].name}>
     //     <figcaption>${imgStore[0].food}</figcaption>
     //   </figure>`)
-    res.json(imgStore);
+
+    //Local Storage
+    let foodArr = JSON.parse(localStorage.getItem('food'));
+    // res.json(imgStore);
+    res.json(foodArr);
 })
 app.post('/ingredients', function (req, res) {
     searchStore = [];
     console.log('ingredients', req.body);
     const ingredArr = req.body.ingred;
     console.log(ingredArr);
-    imgStore.forEach((value, index) => {
+
+    //Local Storage
+    let foodArr = JSON.parse(localStorage.getItem('food'));
+    foodArr.forEach((value, index) => {
         let count = 0;
         if (value.ingredients.length >= ingredArr.length) {
             for (let i = 0; i < value.ingredients.length; i++) {
@@ -160,7 +195,31 @@ app.delete('/food/:id', function (req, res) {
         if (item.id === Number(id)) {
             imgStore.splice(index, 1);
         }
+    });
+    //Local Storage
+    let foodArr = JSON.parse(localStorage.getItem('food'));
+    console.log(foodArr);
+    foodArr.forEach((item, index) => {
+        if (item.id === Number(id)) {
+            foodArr.splice(index, 1);
+        }
+
+    });
+    const updatedFoodArr = foodArr.map((item, index) => {
+        const obj = {};
+        obj['id'] = index + 1;
+        obj['food'] = item.food;
+        obj['image'] = item.image;
+        obj['altName'] = item.altName;
+        obj['ingredients'] = item.ingredients;
+        obj['instruction'] = item.instruction;
+        obj['user'] = item.user;
+        return obj;
     })
+    localStorage.setItem('food', JSON.stringify(updatedFoodArr));
+    let count = JSON.parse(localStorage.getItem("count"));
+    count = updatedFoodArr.length;
+    localStorage.setItem('count', JSON.stringify(count));
     console.log('Reached Delete');
     res.send("Succefully deleted food!");
 })
@@ -177,10 +236,10 @@ function isLoggedIn(req, res, next) {
         res.redirect('/login.html');
     }
 }
-passport.isAuthorized= (userType) => {
+passport.isAuthorized = (userType) => {
     return (req, res, next) => {
         console.log('reached authorized');
-        if(req.user) {
+        if (req.user) {
             if (userType === 1) {
                 // user role is admin
                 // console.log('reached authorized');
@@ -205,7 +264,7 @@ passport.isAuthorized= (userType) => {
         } else {
             return res.status(401).send(`<div style="text-align: center;font-size:50px;"><p>401</p><p>UNAUTHORIZED</p></div>`);
         }
-        
+
     }
 }
 app.get('/manage', passport.isAuthorized(1), function (req, res) {
@@ -217,7 +276,7 @@ app.get('/manage', passport.isAuthorized(1), function (req, res) {
     //     res.redirect('/manage.html');
     // }
     res.redirect('/manage.html');
-    
+
 })
 db.sequelize.sync({
     force: false
