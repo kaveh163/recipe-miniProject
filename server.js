d589780fbc2b48a7928c3c7b1744dc34c9bf96df
Kaveh <kavehfrounchi@gmail.com>
2021-12-01 21:44:03 +0330

    Changed the PORT

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (typeof localStorage === "undefined" || localStorage === null) {
 const app = express();
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 let arrStore = [];
 let imgStore = [];
 let searchStore = [];
