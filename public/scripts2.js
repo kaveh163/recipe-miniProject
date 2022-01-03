0b57ebd3b19275f72f717add3dda2d25e10a3486
Kaveh <kavehfrounchi@gmail.com>
2022-01-04 02:30:13 +0330

    updated the user interface of the website

--- public/scripts2.js
+++ public/scripts2.js
@@ -8,24 +8,36 @@ $(function() {
             $('#recipes').empty();
             data.forEach((item, index)=> {
                 HTML += `<figure>
-                <img src= ${item.image} alt=${item.altName} style="width:100%;height:250px">
-                <figcaption><h3 class="fontRes">Fig.${item.id} - ${item.food}.</h3></figcaption>
+                <img src= ${item.image} alt=${item.altName} style="width:100%;height:250px;object-fit: cover">
+                <figcaption><h3 class="fontRes" style="font-family: 'Times New Roman', serif;"><span>${item.food}</span></h3></figcaption>
               </figure>`;
-              HTML += '<div class="d-flex justify-content-center">'
-              HTML += '<div class="text-center">'
-              HTML += `<label style="font-family: Comic Sans MS, Comic Sans, cursive" class="d-block"> ingredients: </label>`
-              HTML += '<ol>'
+            //   HTML += '<div class="d-flex justify-content-center">'
+              HTML += '<div class="text-left">'
+              HTML += `<label style="font-family: Comic Sans MS, Comic Sans, cursive" class="d-block"> 
+              <span class="bg-danger text-white" style="border: 1px solid black; border-radius:25px; padding: 10px;">ingredients </span></label>`
+            //   card deck section
+             HTML += '<div class="card-deck mt-3">'
               item.ingredients.forEach((value, index)=> {
+                  HTML += '<div class="d-flex justify-content-start">'
+                // HTML += '<div class="card-deck">'
+                HTML += '<div class="card bg-primary" style="border-radius:50%">'
+                HTML += '<div class="card-body text-center text-white">'
                 
-                  HTML +=`<li>${value}</li>`
+                  HTML +=`<p class="card-text">${value}</p>`
+                  HTML += '</div>'
+                  HTML += '</div>'
+                //   HTML += '</div>'
+                  HTML+= '</div>'
                   
               })
-              HTML += '</ol>'
-              HTML += `<p style="font-family: Comic Sans MS, Comic Sans, cursive;text-align: center;">instructions:</p>`
-              HTML += `<p style="font-family: Comic Sans MS, Comic Sans, cursive;text-align:justify;text-align-last: center;">${item.instruction}</p>`
-              HTML += `<p><b style="font-family: Comic Sans MS, Comic Sans, cursive">Created by:</b> ${item.user}</p>`
+              HTML+= '</div>'
+             HTML += '<hr>'
+            //   end of card deck section
+              HTML += `<p class="mt-4"style="font-family: Comic Sans MS, Comic Sans, cursive;text-align: left;"><span class="bg-info text-white" style="border: 1px solid black; border-radius:25px; padding: 10px;">instructions</span></p>`
+              HTML += `<textarea disabled class="form-control" style="font-family: Comic Sans MS, Comic Sans, cursive;text-align:justify;text-align-last: left;overflow: auto"rows="5">${item.instruction}</textarea>`
+              HTML += `<p class="mt-5"><b style="font-family: Comic Sans MS, Comic Sans, cursive">Created by:</b> ${item.user}</p>`
               HTML += '</div>'
-              HTML += '</div>'
+            //   HTML += '</div>'
             })
             $('#recipes').append(HTML);
         },
