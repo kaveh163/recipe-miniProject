a3a57773c698fdf3a700ee9be58acf1eef0df78e
Kaveh <kavehfrounchi@gmail.com>
2022-01-19 19:47:02 +0330

    Changed how the ingredients are shown to the user in mobile mode and laptop

--- public/search.js
+++ public/search.js
@@ -73,8 +73,9 @@ $(function () {
               <span class="bg-danger text-white" style="border: 1px solid black; border-radius:25px; padding: 10px;">ingredients </span></label>`
             //   card deck section
              HTML += '<div class="card-deck mt-3">'
+             HTML += '<div class="d-inline-flex justify-content-start flex-wrap">'
               item.ingredients.forEach((value, index)=> {
-                  HTML += '<div class="d-flex justify-content-start">'
+                  HTML += '<div class="">'
                 // HTML += '<div class="card-deck">'
                 HTML += '<div class="card bg-primary" style="border-radius:50%">'
                 HTML += '<div class="card-body text-center text-white">'
@@ -87,11 +88,12 @@ $(function () {
                   
               })
               HTML+= '</div>'
+              HTML+= '</div>'
              HTML += '<hr>'
             //   end of card deck section
               HTML += `<p class="mt-4"style="font-family: Comic Sans MS, Comic Sans, cursive;text-align: left;"><span class="bg-info text-white" style="border: 1px solid black; border-radius:25px; padding: 10px;">instructions</span></p>`
               HTML += `<textarea disabled class="form-control" style="font-family: Comic Sans MS, Comic Sans, cursive;text-align:justify;text-align-last: left;overflow: auto"rows="5">${item.instruction}</textarea>`
-              HTML += `<p class="mt-5"><b style="font-family: Comic Sans MS, Comic Sans, cursive">Created by:</b> ${item.user}</p>`
+              HTML += `<p class="mt-4 mb-5"><b style="font-family: Comic Sans MS, Comic Sans, cursive">Created by:</b> ${item.user}</p>`
               HTML += '</div>'
             //   HTML += '</div>'
                         })
